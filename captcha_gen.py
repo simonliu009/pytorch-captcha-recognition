b7d81edf90f7a8406237af396ac8787f324e9548
Simon Liu <sliu009@gmail.com>
2022-04-24 07:27:24 +0000

    1.生成训练验证码改为5位长度+小写字母 2.gitignore 增加dataset文件夹

--- captcha_gen.py
+++ captcha_gen.py
@@ -6,6 +6,19 @@ import time
 import captcha_setting
 import os
 
+# number = ['0','1','2','3','4','5','6','7','8','9']
+# alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+# ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+# 验证码一般都无视大小写；验证码长度4个字符 这里为节省演示时间 将alphabet ALPHABET注释掉
+# def random_captcha_text(char_set=number+alphabet+ALPHABET, captcha_size=5):
+# # def random_captcha_text(char_set=number, captcha_size=4):
+#     captcha_text = []
+#     for i in range(captcha_size):
+#         c = random.choice(char_set)
+#         captcha_text.append(c)
+#     return ''.join(captcha_text)
+
 def random_captcha():
     captcha_text = []
     for i in range(captcha_setting.MAX_CAPTCHA):
@@ -17,11 +30,12 @@ def random_captcha():
 def gen_captcha_text_and_image():
     image = ImageCaptcha()
     captcha_text = random_captcha()
+    # captcha_text = random_captcha_text()
     captcha_image = Image.open(image.generate(captcha_text))
     return captcha_text, captcha_image
 
 if __name__ == '__main__':
-    count = 30
+    count = 1000
     path = captcha_setting.TRAIN_DATASET_PATH    #通过改变此处目录，以生成 训练、测试和预测用的验证码集
     if not os.path.exists(path):
         os.makedirs(path)
--- captcha_setting.py
+++ captcha_setting.py
@@ -2,14 +2,23 @@
 import os
 # 验证码中的字符
 # string.digits + string.ascii_uppercase
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
 NUMBER = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-ALL_CHAR_SET = NUMBER + ALPHABET
+ALL_CHAR_SET = NUMBER + alphabet + ALPHABET
+# ALL_CHAR_SET = NUMBER + ALPHABET
 ALL_CHAR_SET_LEN = len(ALL_CHAR_SET)
-MAX_CAPTCHA = 4
+MAX_CAPTCHA = 5
 
 # 图像大小
+# IMAGE_HEIGHT = 224
+# IMAGE_WIDTH = 224
+# IMAGE_HEIGHT = 100
+# IMAGE_WIDTH = 210
+
 IMAGE_HEIGHT = 60
 IMAGE_WIDTH = 160
 
